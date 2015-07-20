897506034dd46ccee1e007adb225e332523d4e11
陆议 <fruit.wj@taobao.com>
2015-07-20 15:52:31 +0800

    增加没有less文件判断

--- README.md
+++ README.md
@@ -13,13 +13,12 @@ npm install --save-dev mat-less
 ```javascript
 var mat  = require('mat')
 var less = require('mat-less')
-var rewrite = require('mat-rewrite')
 
 mat.task('less', function () {
   mat.url([/\.css/])
-    .use(rewrite([
+    .rewrite([
       [/\.css/g, '.less']
-    ]))
+    ])
     .use(less())
 })
 ```
--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 var nodeless  = require('less')
 var path      = require('path')
+var Stream    = require('stream')
 var normalize = path.normalize
 var resolve   = path.resolve
 var dirname   = path.dirname
 var join      = path.join
-
-function cobody(stream) {
-  return function(cb){
-    var buffers = []
-    stream.on('data', function(chunk){
-      buffers.push(chunk)
-    })
-    stream.on('end', function(){
-      cb(null, Buffer.concat(buffers).toString('utf-8'))
-    })
-  }
-}
 
 function coless(body, config) {
   return function(cb) {
@@ -32,6 +21,12 @@ function less(rules) {
   return function *less(next) {
     yield next
 
+    var body = this.body.toString()
+
+    if (body == 'Not Found') {
+      throw new Error('路径：' + this.path + ' 对应的文件没有找到')
+    }
+
     var path = this.path
     var root = this.app.root
     root = normalize(resolve(root))
@@ -40,7 +35,6 @@ function less(rules) {
     var config = {
       paths: [path]
     }
-    var body = yield cobody(this.body)
     this.body = yield coless(body, config)
     this.type = 'text/css'
   }
