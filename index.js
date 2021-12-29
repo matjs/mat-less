9470f5e29e2a9021d1ab6eee18d74d9a0ce41c26
yisibl <percyley@qq.com>
2021-12-29 11:50:52 +0800

    feat: 升级到 less v4

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var nodeless  = require('less')
-var path      = require('path')
-var Stream    = require('stream')
-var normalize = path.normalize
-var resolve   = path.resolve
-var dirname   = path.dirname
-var join      = path.join
+const path      = require('path')
+const normalize = path.normalize
+const resolve   = path.resolve
+const dirname   = path.dirname
+const join      = path.join
+const nodeless  = require('less')
 
-function coless(body, config) {
+function renderLess(body, config) {
   return function(cb) {
     nodeless.render(body, config, function (err, css) {
       if (err) {
@@ -21,14 +20,14 @@ function less(opts) {
   return function *less(next) {
     yield next
 
-    var body = this.body.toString()
+    const body = this.body.toString()
 
     if (body == 'Not Found') {
       throw new Error('路径：' + this.path + ' 对应的文件没有找到')
     }
 
-    var path = this.path
-    var root = this.app.root
+    let path = this.path
+    let root = this.app.root
     root = normalize(resolve(root))
     path = normalize(join(root, path))
     path = dirname(path)
@@ -36,7 +35,7 @@ function less(opts) {
     opts = opts || {}
     opts.paths = [path]
     
-    this.body = yield coless(body, opts)
+    this.body = yield renderLess(body, opts)
     this.type = 'text/css'
   }
 }
